53ba27e57d8a6b3cb7bac3ef9b7f9b97462ec093
Gael <gaelangel.gga@gmail.com>
2025-03-17 01:31:54 -0400

    Implemented piece class

--- src/chess/Board.java
+++ src/chess/Board.java
@@ -1,18 +1,50 @@
 package chess;
 
+import static chess.Utilities.toSquarePosition;
+
 public class Board {
-  private long[] bitboards;
+  private final long[] bitboards;
 
   public Board() {
     this("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
   }
 
   public Board(String fen) {
-    this.bitboards = new long[12];
+    this.bitboards = new long[Piece.values().length];
     loadPosition(fen);
   }
 
-  private void loadPosition(String fen) {
+  public void loadPosition(String fen) {
+    // reset bitboards
+    for (int i = 0; i < 12; i++) {
+      bitboards[i] = 0L;
+    }
 
+    String[] fenParams = fen.split(" ");
+
+    // set pieces on their corresponding bitboards
+    char[] fenBoard = fenParams[0].toCharArray();
+    int rank = 0, file = 0;
+    for (char symbol : fenBoard) {
+      if (symbol == '/') {
+        file = 0;
+        rank++;
+      } else if (Character.isDigit(symbol)) {
+        file += Character.getNumericValue(symbol);
+      } else {
+        Piece type = Piece.fromChar(symbol);
+        long bitboard = getPieces(type);
+
+//        Piece type = getPieceFromSymbol(symbol);
+//        long bitboard = getBitboardPiece(pieceType);
+//        int square = toSquarePosition(rank, file);
+//        bitboardPieces[pieceType.ordinal()] = setBit(bitboard, square);
+        file++;
+      }
+    }
+  }
+
+  private long getPieces(Piece type) {
+    return bitboards[type.index()];
   }
 }
---
+++ src/chess/Color.java
@@ -0,0 +1,6 @@
+package chess;
+
+public enum Color {
+  WHITE, // 0
+  BLACK  // 1
+}
